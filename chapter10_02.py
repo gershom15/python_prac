2b9cc627481aead62e6f880c648690bc8e79127f
why <sypark150208@gmail.com>
2022-03-07 17:03:45 +0900

    Project : Making HangMan Game

---
+++ chapter10_02.py
@@ -0,0 +1,103 @@
+## HangMan 미니게임 제작
+# 프로그램 완성 및 최종 테스트
+
+import time
+#csv 처리
+import csv
+# random
+import random
+# 사운드 처리
+import winsound
+
+
+# 처음 인사
+name = input('What is your name?')
+
+print('Hi, ' + name, 'Time to play Hangman game!')
+
+print()
+
+time.sleep(1)  # 좀더 스무스하게 보이기 위해서
+
+print('Start Loading...')
+print()
+time.sleep(0.5)
+
+
+# csv 단어 리스트
+words = []
+
+# 문제 csv파일 로드
+with open('./resource/word_list.csv',) as f:
+    reader = csv.reader(f)
+    # header skip
+    next(reader)
+    for c in reader:
+        words.append(c)
+
+# 라스트 섞기
+random.shuffle(words)
+
+q = random.choice(words)
+
+
+# 정답 단어
+word = q[0].strip()     # .strip()은 양쪽에 공백을 제거함
+
+# 추측 단어
+guesses = ''
+
+# 기회
+turns = 10
+
+# 핵심 : While Loop
+# 찬스 카운트 남아있을 경우
+while turns > 0:
+    # 실패 횟수(단어 매치 수)
+    failed = 0
+
+    # 정답 단어 반복
+    for char in word:
+        # 정답 단어 내에 추측 문자가 포함되어 있는 경우
+        if char in guesses:
+            # 추측 단어 출력
+            print(char, end=' ')
+        else:
+            # 틀린 경우는 대시로 처리
+            print('_', end=' ')
+            failed += 1
+
+    # 단어 추측이 성공한 경우
+    if failed == 0:
+        print()
+        print()
+        # 성공 사운드
+        winsound.PlaySound('./sound/good.wav', winsound.SND_FILENAME) # 파일네임을 그대로 재생
+        print('Congratulations! The Guesses is correct.')
+        # While문 중단
+        break
+    print()
+
+    ## 본격적으로 게임 시작
+    # 추측 단어 문자 단위 입력
+    print()
+    print('Hint : {}'.format(q[1]).strip())
+    guess = input('Guess a character.')
+
+    # 단어 더하기
+    guesses += guess
+
+    # 정답 단어에 추측한 문자가 포함되어 있지 않으면
+    if guess not in word:
+        # 기회 횟수 감:
+        turns -= 1
+        # 오류 메시지
+        print('Oops! Wrong')
+        # 남은 기회 출력
+        print('You have ', turns, 'more guesses!')
+
+        if turns == 0 :
+            # 실패 사운드
+            winsound.PlaySound('./sound/bad.wav', winsound.SND_FILENAME)
+            # 실패 메시지
+            print('You hangman game failed. Bye!')
